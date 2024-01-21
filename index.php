0c0f93a5433e418334946f86dcdb02b515445c0d
Emillio Devista Eqhar S <156658457+AT12394@users.noreply.github.com>
2024-01-21 14:35:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kos Amanah</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+		<header>
+            <h1>Amanah Kos</h1>
+            <nav>
+                <ul>
+                    <li><a href="#Profile">Profile</a></li>
+                    <li><a href="Booking.php">Booking</a></li>
+                    <li><a href="#Contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+
+		<div class="content">
+            <section id="conten-1">
+                <img src="rumah.jpg">
+                <h1>INFO KOST DI BANDUNG FASILITAS LENGKAP</h1>
+                <h2>Selamat Datang di Amanah Kos</h2>
+                <p>
+                    Apakah Anda mencari tempat tinggal yang nyaman,
+                     aman, dan terjangkau? Amanah Kos adalah pilihan
+                      ideal untuk Anda! Kami menawarkan hunian yang menyajikan 
+                      keseimbangan sempurna antara kenyamanan, kebersihan, dan kemudahan akses
+                </p>
+            </section>
+            <section id="conten-2">
+                <h1 id="Profile">Mengapa harus Amanah Kos</h1>
+                <p>
+                    Amanah Kos menawarkan berberpa pertimbangan untuk
+                    yang sedang mencari hunian untuk wilayah Bandung 
+                    Barat. Berikut fasilitas yang di tawarkan;
+                </p>
+                <ul>
+                    <li>Fasilitas Premium</li>
+                    <li>Lokasi Strategis</li>
+                    <li>Lingkungan Bersih Teratur</li>
+                    <li>Keamanan Terjamin</li>
+                </ul>
+                <div class="album">
+                    <img src="fs2.jpeg" width="500" height="300">
+                </div>
+            </section>
+            <section id="conten-3">
+                <h1 id="Contact">Contact</h1>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1980.7481505261787!2d107.45528479494493!3d-6.830939174609519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68fdff29879ee7%3A0x17bed16bbd3d5ea5!2sJl.%20Nasional%20III%20No.20%2C%20Ciburuy%2C%20Kec.%20Padalarang%2C%20Kabupaten%20Bandung%20Barat%2C%20Jawa%20Barat%2040553!5e0!3m2!1sid!2sid!4v1705727003404!5m2!1sid!2sid" width="600" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </section>
+           
+        </div>
+</body>
+</html>
